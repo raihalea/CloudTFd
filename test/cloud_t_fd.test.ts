d63429ddf2fbba36d561ae8efa9eb7621ebc0fd0
raiha <68338762+raihalea@users.noreply.github.com>
2023-10-29 20:30:24 +0900

    Initial commit

---
+++ test/cloud_t_fd.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudTFd from '../lib/cloud_t_fd-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud_t_fd-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudTFd.CloudTFdStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
