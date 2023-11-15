1f6999793eea1080e03294a5c8c70a0a9ef6f5be
raiha <68338762+raihalea@users.noreply.github.com>
2023-11-16 02:41:07 +0900

    update

--- lib/cloudtfd-stack.ts
+++ lib/cloudtfd-stack.ts
@@ -1,46 +1,92 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import * as rds from "aws-cdk-lib/aws-rds";
 import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
 import * as elasticache from "aws-cdk-lib/aws-elasticache";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as ecrAssets from "aws-cdk-lib/aws-ecr-assets";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import { NoOutboundTrafficSecurityGroup } from "./default-security-group";
+import { AwsManagedPrefixList } from "./aws-managed-prefix-list";
 
 export class CloudTFdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "VPC", {
-      natGateways: 0,
-    });
+    // prettier-ignore
+    const vpc = new ec2.Vpc(this, "Vpc", { natGateways: 0,restrictDefaultSecurityGroup: true});
 
-    vpc.addGatewayEndpoint("s3_endpoint", {
+    const s3Endpoint = vpc.addGatewayEndpoint("S3Endpoint", {
       service: ec2.GatewayVpcEndpointAwsService.S3,
     });
-    vpc.addInterfaceEndpoint("ecr_endpoint", {
-      service: ec2.InterfaceVpcEndpointAwsService.ECR,
-    });
-    vpc.addInterfaceEndpoint("ecrdkr_endpoint", {
-      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+
+    // prettier-ignore
+    const endpointOptionsForECS: {[name: string]: ec2.InterfaceVpcEndpointOptions;} = {
+      EcrEndpoint: {
+        service: ec2.InterfaceVpcEndpointAwsService.ECR,
+        // lookupSupportedAzs: true,
+        // subnets: {subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS}
+      },
+      EcrdkrEndpoint: {
+        service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+        // lookupSupportedAzs: true,
+        // subnets: {subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS}
+      },
+      LogsEndpoint: {
+        service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+        // lookupSupportedAzs: true,
+        // subnets: {subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS}
+      },
+      SsmEndpoint: {
+        service: ec2.InterfaceVpcEndpointAwsService.SSM,
+        // lookupSupportedAzs: true,
+        // subnets: {subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS}
+      },
+      SsmMessagesEndpoint: {
+        service: ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES,
+        // lookupSupportedAzs: true,
+        // subnets: {subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS}
+      },
+      SecretsManagerEndpoint: {
+        service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+        // lookupSupportedAzs: true,
+        // subnets: {subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS}
+      },
+    };
+
+    const endpointsForECS: ec2.InterfaceVpcEndpoint[] = [];
+    for (const [name, options] of Object.entries(endpointOptionsForECS)) {
+      const endpoint = vpc.addInterfaceEndpoint(name, options);
+      endpointsForECS.push(endpoint);
+    }
+
+    const bucket = new s3.Bucket(this, "S3", {
+      enforceSSL: true,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    vpc.addInterfaceEndpoint("logs_endpoint", {
-      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+    const user = new iam.User(this, "S3User");
+    const s3AccessKey = new iam.AccessKey(this, "S3AccessKey", { user });
+    // prettier-ignore
+    const s3SecretAccessKey = new secretsmanager.Secret(this, "S3SecretAccessKey", {
+      secretStringValue: s3AccessKey.secretAccessKey,
     });
-    vpc.addInterfaceEndpoint("ssm_endpoint", {
-      service: ec2.InterfaceVpcEndpointAwsService.SSM,
-    });
-    vpc.addInterfaceEndpoint("ssmmessages_endpoint", {
-      service: ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES,
-    });
-    vpc.addInterfaceEndpoint("secretsmanager_endpoint", {
-      service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
-    });
+    bucket.grantReadWrite(user);
 
     const DB_USERNAME = "ctfd";
 
-    const db_cluster = new rds.DatabaseCluster(this, "Database", {
+    // prettier-ignore
+    const dbClusterSecurityGroup = new NoOutboundTrafficSecurityGroup(
+      this, "DbSecurityGroup", { vpc,}
+    );
+
+    const dbCluster = new rds.DatabaseCluster(this, "Db", {
       engine: rds.DatabaseClusterEngine.auroraMysql({
         version: rds.AuroraMysqlEngineVersion.VER_3_04_0,
       }),
@@ -58,29 +104,18 @@ export class CloudTFdStack extends cdk.Stack {
         subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
       },
       vpc,
+      securityGroups: [dbClusterSecurityGroup],
       serverlessV2MaxCapacity: 32,
       serverlessV2MinCapacity: 0.5,
       storageEncrypted: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY, //
     });
 
-    // const db_pass = cdk.SecretValue.secretsManager(
-    //   db_cluster.secret!.secretArn,
-    //   { jsonField: "password" }
-    // ).unsafeUnwrap();
-    // const database_url = new secretsmanager.Secret(this, "db_secrets", {
-    //   secretStringValue: cdk.SecretValue.unsafePlainText(
-    //     `mysql+pymysql://${DB_USERNAME}:${db_pass}@${db_cluster.clusterEndpoint.socketAddress}/ctfd`
-    //   ),
-    // });
+    // prettier-ignore
+    const redisSG = new NoOutboundTrafficSecurityGroup(
+      this, "RedisSecurityGroup", { vpc });
 
-    const redisSG = new ec2.SecurityGroup(this, "RedisSecurityGroup", {
-      vpc,
-      description: "Allow redis access from server",
-      allowAllOutbound: false,
-    });
-
-    const subnetGroup = new elasticache.CfnSubnetGroup(
+    const redisSubnetGroup = new elasticache.CfnSubnetGroup(
       this,
       "ClusterSubnetGroup",
       {
@@ -92,7 +127,7 @@ export class CloudTFdStack extends cdk.Stack {
 
     const REDIS_USER = "ctfd";
     const REDIS_PORT = 6379;
-    const redis_auth = new secretsmanager.Secret(this, "RedisSecret", {
+    const redisAuth = new secretsmanager.Secret(this, "RedisSecret", {
       generateSecretString: {
         secretStringTemplate: JSON.stringify({ username: REDIS_USER }),
         generateStringKey: "password",
@@ -112,24 +147,28 @@ export class CloudTFdStack extends cdk.Stack {
         replicationGroupDescription: "redis cache",
         // engineVersion: '5.0.6', // ARM instance requires minimum Redis for ElastiCache 5.0.6 version.
         securityGroupIds: [redisSG.securityGroupId],
-        cacheSubnetGroupName: subnetGroup.cacheSubnetGroupName,
+        cacheSubnetGroupName: redisSubnetGroup.cacheSubnetGroupName,
         automaticFailoverEnabled: true,
         multiAzEnabled: true,
         atRestEncryptionEnabled: true,
         transitEncryptionEnabled: true,
         transitEncryptionMode: "required",
-        authToken: cdk.SecretValue.secretsManager(redis_auth.secretArn, {
+        authToken: cdk.SecretValue.secretsManager(redisAuth.secretArn, {
           jsonField: "password",
         }).unsafeUnwrap(),
         port: REDIS_PORT,
       }
     );
-    elasticache_redis.addDependency(subnetGroup);
+    elasticache_redis.addDependency(redisSubnetGroup);
 
-    const cluster = new ecs.Cluster(this, "Cluster", {
-      vpc,
-    });
+    // prettier-ignore
+    const ecsSecurityGroup = new NoOutboundTrafficSecurityGroup(
+      this, "EcsSecurityGroup", { vpc });
 
+    // prettier-ignore
+    const cluster = new ecs.Cluster(this, "Cluster", { vpc });
+
+    // prettier-ignore
     const loadBalancedFargateService =
       new ecsPatterns.ApplicationLoadBalancedFargateService(this, "Service", {
         cluster,
@@ -137,11 +176,16 @@ export class CloudTFdStack extends cdk.Stack {
         desiredCount: 1,
         cpu: 512,
         taskImageOptions: {
-          image: ecs.ContainerImage.fromAsset("./CTFd"),
+          image: ecs.ContainerImage.fromAsset("./CTFd", {
+            platform: ecrAssets.Platform.LINUX_ARM64
+          }),
           environment: {
+            UPLOAD_PROVIDER: "s3",
+            AWS_ACCESS_KEY_ID: s3AccessKey.accessKeyId,
+            AWS_S3_BUCKET: bucket.bucketName,
             DATABASE_USER: DB_USERNAME,
-            DATABASE_HOST: db_cluster.clusterEndpoint.hostname,
-            DATABASE_PORT: String(db_cluster.clusterEndpoint.port),
+            DATABASE_HOST: dbCluster.clusterEndpoint.hostname,
+            DATABASE_PORT: String(dbCluster.clusterEndpoint.port),
             REDIS_PROTOCOL: "rediss",
             REDIS_HOST: elasticache_redis.attrPrimaryEndPointAddress,
             REDIS_PORT: elasticache_redis.attrPrimaryEndPointPort,
@@ -150,38 +194,63 @@ export class CloudTFdStack extends cdk.Stack {
             REVERSE_PROXY: "true",
           },
           secrets: {
-            DATABASE_PASSWORD: ecs.Secret.fromSecretsManager(
-              db_cluster.secret!,
-              "password"
-            ),
-            REDIS_PASSWORD: ecs.Secret.fromSecretsManager(
-              redis_auth,
-              "password"
-            ),
+            AWS_SECRET_ACCESS_KEY: ecs.Secret.fromSecretsManager( s3SecretAccessKey ),
+            DATABASE_PASSWORD: ecs.Secret.fromSecretsManager( dbCluster.secret!, "password"),
+            REDIS_PASSWORD: ecs.Secret.fromSecretsManager( redisAuth, "password" ),
           },
           containerPort: 8000,
         },
+        runtimePlatform: {
+          cpuArchitecture: ecs.CpuArchitecture.ARM64,
+          operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
+        },
         enableExecuteCommand: true,
+        // openListener: false,
+        securityGroups: [ecsSecurityGroup],
         // taskSubnets: {
         //   subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         // },
       });
 
-    db_cluster.connections.allowFrom(
+    loadBalancedFargateService.targetGroup.configureHealthCheck({
+      path: "/healthcheck",
+    });
+
+    loadBalancedFargateService.service.node.addDependency(elasticache_redis);
+
+    endpointsForECS.forEach((endpoint) => {
+      endpoint.connections.allowFrom(ecsSecurityGroup, ec2.Port.tcp(443));
+    });
+
+    // prettier-ignore
+    const s3PrefixList = new AwsManagedPrefixList( this, "CloudfrontOriginPrefixList",
+      { name: `com.amazonaws.${this.region}.s3` }
+    ).prefixList;
+
+    ecsSecurityGroup.addEgressRule(
+      ec2.Peer.prefixList(s3PrefixList.prefixListId),
+      ec2.Port.tcp(443)
+    );
+
+    dbCluster.connections.allowFrom(
       loadBalancedFargateService.service,
-      ec2.Port.tcp(db_cluster.clusterEndpoint.port),
+      ec2.Port.tcp(dbCluster.clusterEndpoint.port),
       "Allow inbound DB connection"
     );
 
-    loadBalancedFargateService.service.node.addDependency(elasticache_redis);
     redisSG.connections.allowFrom(
       loadBalancedFargateService.service,
       ec2.Port.tcp(elasticache_redis.port!),
       "Allow inbound Redis connection"
     );
 
-    loadBalancedFargateService.targetGroup.configureHealthCheck({
-      path: "/healthcheck",
+    new cloudfront.Distribution(this, "CloudFront", {
+      defaultBehavior: {
+        origin: new origins.LoadBalancerV2Origin(
+          loadBalancedFargateService.loadBalancer,
+          { protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY }
+        ),
+      },
     });
   }
 }
