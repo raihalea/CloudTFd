bfa6c9751a4a4b9b37e82b84241ea57bc723af31
raiha <68338762+raihalea@users.noreply.github.com>
2023-11-30 01:18:53 +0900

    update waf

---
+++ lib/config/wafManagedRule-config.ts
@@ -0,0 +1,75 @@
+import { CfnWebACL } from "aws-cdk-lib/aws-wafv2";
+
+// type listOfRules = {
+//   name: string;
+//   priority?: number;
+//   overrideAction: string;
+//   excludedRules: string[];
+//   scopeDownStatement?: CfnWebACL.StatementProperty;
+// };
+
+interface listOfRules {
+  name: string;
+  priority?: number;
+  overrideAction: string;
+  excludedRules: string[];
+  scopeDownStatement?: CfnWebACL.StatementProperty;
+}
+
+export const managedRules: listOfRules[] = [
+  {
+    name: "AWSManagedRulesCommonRuleSet",
+    // priority: 20,
+    overrideAction: "none",
+    excludedRules: ["SizeRestrictions_BODY"],
+    // scopeDownStatement: startsWith("/admin"),
+  },
+  {
+    name: "AWSManagedRulesAmazonIpReputationList",
+    // priority: 20,
+    overrideAction: "none",
+    excludedRules: [],
+  },
+  {
+    name: "AWSManagedRulesKnownBadInputsRuleSet",
+    // priority: 30,
+    overrideAction: "none",
+    excludedRules: [],
+  },
+  {
+    name: "AWSManagedRulesAnonymousIpList",
+    // priority: 40,
+    overrideAction: "none",
+    excludedRules: [],
+  },
+  {
+    name: "AWSManagedRulesLinuxRuleSet",
+    // priority: 50,
+    overrideAction: "none",
+    excludedRules: [],
+  },
+  {
+    name: "AWSManagedRulesSQLiRuleSet",
+    // priority: 60,
+    overrideAction: "none",
+    excludedRules: [],
+  },
+];
+
+export function startsWith(path: string): CfnWebACL.StatementProperty {
+  return {
+    byteMatchStatement: {
+      fieldToMatch: {
+        uriPath: {},
+      },
+      positionalConstraint: "STARTS_WITH",
+      searchString: path,
+      textTransformations: [
+        {
+          priority: 0,
+          type: "NONE",
+        },
+      ],
+    },
+  };
+}
